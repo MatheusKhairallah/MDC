467fdd407a6aa3db12fe7513f97cf85254eaa610
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-07-03 21:49:09 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MDC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1;
+            int n2;
+            int dividendo;
+            int divisor;
+            int resto;
+            
+            Console.Clear();
+            
+            Console.WriteLine("Máximo Divisor Comum (método iterativo)");
+            Console.Write("Introduza o primeiro número: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Introduza o primeiro número: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            if(n1 == 0 || n2 ==0)
+            Console.WriteLine("Não pode haver valores 0");
+
+            if (n1>n2)
+            {
+              dividendo = n1;
+              divisor = n2;  
+            }
+
+            else
+            {
+               dividendo = n2;
+               divisor = n1;  
+            }
+            while (dividendo % divisor != 0)
+            {
+                resto = dividendo % divisor;
+                dividendo = divisor;
+                divisor = resto;
+            }
+            Console.WriteLine("MDC=" + divisor);
+
+
+
+
+        }
+    }
+}
